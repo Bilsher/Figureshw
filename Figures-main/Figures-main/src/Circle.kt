7f356b80cb35a8c903d6b0e4757bbb70f1a2520d
Egor <58413305+Bilsher@users.noreply.github.com>
2021-11-25 15:14:40 +0800

    Delete Figures-main directory

--- Figures-main/Figures-main/src/Circle.kt
+++
@@ -1,7 +0,0 @@
-// TODO: дополнить определение класса размерами и позицией
-class Circle : Figure(0) {
-    // TODO: реализовать интерфейс Transforming
-    override fun area(): Float {
-        return  0.toFloat();
-    }
-}
